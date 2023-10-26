f2fdaf1cf633a43849f5cb2a7fc5e443a2b209fe
Shubham soni <ss4185667@gmail.com>
2023-10-26 23:50:12 +0530

    added-questions

--- questions.js
+++ questions.js
@@ -108,5 +108,124 @@ let questions = [
       "Malthus",
       "Humboldt"
     ]
-  }
+  },
+  {
+    numb: 11,
+    question: "For more diversity, the level of disturbance should be",
+    answer: "intermediate",
+    options: [
+      "less",
+      "intermediate",
+      "more",
+      "none of these"
+    ]
+},
+
+{
+    numb: 12,
+    question: "Hierarchy emerges almost inevitably through a wide variety of evolutionary processes, for the simple reason that hierarchical structures are _____ (fill in the blank)",
+    answer: "stable",
+    options: [
+      "perfect",
+      "imperfect",
+      "stable",
+      "unstable"
+    ]
+},
+
+{
+    numb: 13,
+    question: "\"Groups of actually or potentially interbreeding natural populations which are reproductively isolated from other such groups\" is a definition of",
+    answer: "species",
+    options: [
+      "cells",
+      "species",
+      "ecosystems",
+      "biomes"
+    ]
+},
+
+{
+    numb: 14,
+    question: "The emergent principle can be stated as",
+    answer: "whole > sum of parts",
+    options: [
+      "whole = sum of parts",
+      "whole < sum of parts",
+      "whole > sum of parts",
+      "none of these"
+    ]
+},
+
+{
+    numb: 15,
+    question: "There is more biodiversity in areas with",
+    answer: "more competition, more predation",
+    options: [
+      "less competition, less predation",
+      "less competition, more predation",
+      "more competition, more predation",
+      "more competition, less predation"
+    ]
+},
+
+{
+    numb: 16,
+    question: "The mitochondrion is a/an",
+    answer: "sub-cellular organelle",
+    options: [
+      "sub-cellular organelle",
+      "cell",
+      "tissue",
+      "organ"
+    ]
+},
+
+{
+    numb: 17,
+    question: "\"The diversity that exists among different geographies\" is",
+    answer: "gamma biodiversity",
+    options: [
+      "alpha biodiversity",
+      "beta biodiversity",
+      "gamma biodiversity",
+      "delta biodiversity"
+    ]
+},
+
+{
+    numb: 18,
+    question: "\"The diversity that exists within an ecosystem\" is",
+    answer: "alpha biodiversity",
+    options: [
+      "alpha biodiversity",
+      "beta biodiversity",
+      "gamma biodiversity",
+      "delta biodiversity"
+    ]
+},
+
+{
+    numb: 19,
+    question: "The hierarchical system was given by",
+    answer: "Simon",
+    options: [
+      "Simon",
+      "Watson",
+      "Hutchinson",
+      "Humboldt"
+    ]
+},
+
+{
+    numb: 20,
+    question: "The laboratory approach to ecology uses",
+    answer: "experiments",
+    options: [
+      "equation",
+      "models",
+      "observations",
+      "experiments"
+    ]
+}
 ];
