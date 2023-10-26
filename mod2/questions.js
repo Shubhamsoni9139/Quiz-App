8803ad010b7a2a6ea99dc12799b6da631d599b00
Shubham soni <ss4185667@gmail.com>
2023-10-27 00:26:12 +0530

    new

---
+++ mod2/questions.js
@@ -0,0 +1,121 @@
+let questions = [
+    {
+        numb: 1,
+        question: "For more diversity, the level of disturbance should be",
+        answer: "intermediate",
+        options: [
+          "less",
+          "intermediate",
+          "more",
+          "none of these"
+        ]
+    },
+    
+    {
+        numb: 2,
+        question: "Hierarchy emerges almost inevitably through a wide variety of evolutionary processes, for the simple reason that hierarchical structures are _____ (fill in the blank)",
+        answer: "stable",
+        options: [
+          "perfect",
+          "imperfect",
+          "stable",
+          "unstable"
+        ]
+    },
+    
+    {
+        numb: 3,
+        question: "\"Groups of actually or potentially interbreeding natural populations which are reproductively isolated from other such groups\" is a definition of",
+        answer: "species",
+        options: [
+          "cells",
+          "species",
+          "ecosystems",
+          "biomes"
+        ]
+    },
+    
+    {
+        numb: 4,
+        question: "The emergent principle can be stated as",
+        answer: "whole > sum of parts",
+        options: [
+          "whole = sum of parts",
+          "whole < sum of parts",
+          "whole > sum of parts",
+          "none of these"
+        ]
+    },
+    
+    {
+        numb: 5,
+        question: "There is more biodiversity in areas with",
+        answer: "more competition, more predation",
+        options: [
+          "less competition, less predation",
+          "less competition, more predation",
+          "more competition, more predation",
+          "more competition, less predation"
+        ]
+    },
+    
+    {
+        numb: 6,
+        question: "The mitochondrion is a/an",
+        answer: "sub-cellular organelle",
+        options: [
+          "sub-cellular organelle",
+          "cell",
+          "tissue",
+          "organ"
+        ]
+    },
+    
+    {
+        numb: 7,
+        question: "\"The diversity that exists among different geographies\" is",
+        answer: "gamma biodiversity",
+        options: [
+          "alpha biodiversity",
+          "beta biodiversity",
+          "gamma biodiversity",
+          "delta biodiversity"
+        ]
+    },
+    
+    {
+        numb: 8,
+        question: "\"The diversity that exists within an ecosystem\" is",
+        answer: "alpha biodiversity",
+        options: [
+          "alpha biodiversity",
+          "beta biodiversity",
+          "gamma biodiversity",
+          "delta biodiversity"
+        ]
+    },
+    
+    {
+        numb: 9,
+        question: "The hierarchical system was given by",
+        answer: "Simon",
+        options: [
+          "Simon",
+          "Watson",
+          "Hutchinson",
+          "Humboldt"
+        ]
+    },
+    
+    {
+        numb: 10,
+        question: "The laboratory approach to ecology uses",
+        answer: "experiments",
+        options: [
+          "equation",
+          "models",
+          "observations",
+          "experiments"
+        ]
+    }
+]
---
+++ mod3/questions.js
@@ -0,0 +1,121 @@
+let questions = [
+  {
+    "numb": 1,
+    "question": "I observe a bird take a tick out of another bird's head and eat it. In the social context, this behavior is known as",
+    "answer": "allo grooming",
+    "options": [
+      "tick hunting",
+      "auto grooming",
+      "allo grooming",
+      "foraging"
+    ]
+},
+
+{
+    "numb": 2,
+    "question": "The scientific study of animal behavior is known as",
+    "answer": "ethology",
+    "options": [
+      "behaviorism",
+      "ecology",
+      "ethology",
+      "prey-predator dynamics"
+    ]
+},
+
+{
+    "numb": 3,
+    "question": "Trampling of grass due to the movement of animals is an example of",
+    "answer": "amensalism",
+    "options": [
+      "mutualism",
+      "amensalism",
+      "commensalism",
+      "protocooperation"
+    ]
+},
+
+{
+    "numb": 4,
+    "question": "Birds on a giraffe are an example of",
+    "answer": "protocooperation",
+    "options": [
+      "colony",
+      "commensalism",
+      "protocooperation",
+      "allelopathy"
+    ]
+},
+
+{
+    "numb": 5,
+    "question": "I observe a monkey take a tick out of another monkey's head and eat it. In the social context, this behavior would be called",
+    "answer": "allo grooming",
+    "options": [
+      "tick hunting",
+      "auto grooming",
+      "allo grooming",
+      "foraging"
+    ]
+},
+
+{
+    "numb": 6,
+    "question": "Hamilton's rule can be stated as",
+    "answer": "rB > C",
+    "options": [
+      "rB < C",
+      "rB > C",
+      "rB = C",
+      "rB + C = 0"
+    ]
+},
+
+{
+    "numb": 7,
+    "question": "Egrets with buffaloes are an example of",
+    "answer": "commensalism",
+    "options": [
+      "colony",
+      "commensalism",
+      "protocooperation",
+      "allelopathy"
+    ]
+},
+
+{
+    "numb": 8,
+    "question": "The interaction between exotic shrubs and trees through the action of seed predators is an example of",
+    "answer": "apparent competition",
+    "options": [
+      "intraspecific competition",
+      "apparent competition",
+      "disguised competition",
+      "harmonious competition"
+    ]
+},
+
+{
+    "numb": 9,
+    "question": "Harmonious interaction occurs where",
+    "answer": "both participants are unharmed",
+    "options": [
+      "at least one participant is benefited",
+      "at least one participant is unharmed",
+      "both participants are benefited",
+      "both participants are unharmed"
+    ]
+},
+
+{
+    "numb": 10,
+    "question": "An inventory of behaviors exhibited by an animal during a behavior exercise is called",
+    "answer": "ethogram",
+    "options": [
+      "ecogram",
+      "ethogram",
+      "behaviourogram",
+      "animalogram"
+    ]
+}
+]
