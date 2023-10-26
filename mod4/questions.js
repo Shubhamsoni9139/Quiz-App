0b15f229c056e42dc78ca95d7a2aea972977b0c4
Shubham soni <ss4185667@gmail.com>
2023-10-27 01:46:33 +0530

    quizz4 added

---
+++ mod4/questions.js
@@ -0,0 +1,121 @@
+let questions = [
+  {
+    "numb": 11,
+    "question": "Tree → Frugivorous birds → Hawk represents",
+    "answer": "spindle pyramids of numbers",
+    "options": [
+      "upright pyramid of numbers",
+      "inverted pyramid of numbers",
+      "spindle pyramids of numbers",
+      "dumbbell pyramid of numbers"
+    ]
+},
+
+{
+    "numb": 12,
+    "question": "If we all became vegetarians, we'll be able to support our large populations. This can be explained through",
+    "answer": "10% rule",
+    "options": [
+      "10% rule",
+      "1% rule",
+      "trophic cascade",
+      "biodiversity"
+    ]
+},
+
+{
+    "numb": 13,
+    "question": "Consider the food chain: Grass → Grasshopper → Frog → Snake → Hawk. As we move up the food chain,",
+    "answer": "available energy decreases",
+    "options": [
+      "available energy decreases",
+      "available energy increases",
+      "available energy remains the same",
+      "available energy is zero everywhere"
+    ]
+},
+
+{
+    "numb": 14,
+    "question": "Glacial lakes are typical examples of",
+    "answer": "oligotrophic lakes",
+    "options": [
+      "eutrophic lakes",
+      "hypereutrophic lakes",
+      "oligotrophic lakes",
+      "mesotrophic lakes"
+    ]
+},
+
+{
+    "numb": 15,
+    "question": "Trees → Birds → Parasites → Hyperparasites represents",
+    "answer": "inverted pyramid of numbers",
+    "options": [
+      "upright pyramid of numbers",
+      "inverted pyramid of numbers",
+      "spindle pyramid of numbers",
+      "dumbbell pyramid of numbers"
+    ]
+}
+,
+{
+    "numb": 16,
+    "question": "Consider the food chain: Grass → Grasshopper → Frog → Snake → Hawk. In the food chain,",
+    "answer": "Frog is consumer and carnivore",
+    "options": [
+      "Frog is producer",
+      "Frog is consumer and carnivore",
+      "Frog is consumer and herbivore",
+      "Frog is decomposer"
+    ]
+}
+,
+{
+    "numb": 17,
+    "question": "At the compensation point",
+    "answer": "photosynthesis = respiration",
+    "options": [
+      "photosynthesis = respiration",
+      "photosynthesis < respiration",
+      "photosynthesis > respiration",
+      "photosynthesis = 0"
+    ]
+}
+,
+{
+    "numb": 18,
+    "question": "Consider the food chain: Grass → Grasshopper → Frog → Snake → Hawk. In this food chain,",
+    "answer": "More number of grasshoppers than hawks can be supported",
+    "options": [
+      "More number of hawks than grasshoppers can be supported",
+      "More number of grasshoppers than hawks can be supported",
+      "Equal number of hawks and grasshoppers can be supported",
+      "None of these"
+    ]
+}
+,
+{
+    "numb": 19,
+    "question": "Consider the food chain: Grass → Grasshopper → Frog → Snake → Hawk. In this food chain,",
+    "answer": "Hawk is consumer and carnivore",
+    "options": [
+      "Hawk is producer",
+      "Hawk is consumer and carnivore",
+      "Hawk is consumer and herbivore",
+      "Hawk is decomposer"
+    ]
+}
+,
+{
+    "numb": 20,
+    "question": "Net primary productivity is given by",
+    "answer": "APAR X LUE",
+    "options": [
+      "APAR X LUE",
+      "APAR + LUE",
+      "APAR - LUE",
+      "APAR / LUE"
+    ]
+}
+]
