6ca1327ae65414646092369eeaf34dc2a5918f50
Shubham soni <ss4185667@gmail.com>
2023-10-27 02:06:23 +0530

    mod5

---
+++ mod5/questions.js
@@ -0,0 +1,120 @@
+let questions = [
+  {
+    "numb": 21,
+    "question": "Which of these is not a measure of absolute population density?",
+    "answer": "pelt count",
+    "options": [
+      "total count",
+      "pelt count",
+      "capture-recapture method",
+      "removal method"
+    ]
+},
+
+{
+    "numb": 22,
+    "question": "The juvenile mortality rate is the annual number of deaths of juveniles per",
+    "answer": "1000 live births",
+    "options": [
+      "100 births",
+      "1000 births",
+      "100 live births",
+      "1000 live births"
+    ]
+},
+
+{
+    "numb": 23,
+    "question": "_____ is how close the measured values are to the correct values",
+    "answer": "accuracy",
+    "options": [
+      "accuracy",
+      "precision",
+      "bias",
+      "variance"
+    ]
+},
+
+{
+    "numb": 24,
+    "question": "____ employs a simple rule of selecting every kth unit starting with a number chosen at random from 1 to k as the random start",
+    "answer": "systematic sampling",
+    "options": [
+      "simple random sampling",
+      "systematic sampling",
+      "stratified sampling",
+      "multistage sampling"
+    ]
+}
+,
+{
+    "numb": 25,
+    "question": "Cover board surveys are typically used for sampling",
+    "answer": "herpetofauna",
+    "options": [
+      "herpetofauna",
+      "fishes",
+      "large mammals",
+      "carnivores"
+    ]
+},
+
+{
+    "numb": 26,
+    "question": "The minimum replacement level fertility for a population to grow should be greater than",
+    "answer": "2",
+    "options": [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+},
+
+{
+    "numb": 27,
+    "question": "The logistic growth curve, when plotted, appears",
+    "answer": "S shaped",
+    "options": [
+      "I shaped",
+      "J shaped",
+      "S shaped",
+      "O shaped"
+    ]
+}
+,
+{
+    "numb": 28,
+    "question": "Pan traps are used for sampling",
+    "answer": "pollinator insects",
+    "options": [
+      "bees",
+      "butterflies",
+      "non-pollinator insects",
+      "pollinator insects"
+    ]
+},
+
+{
+    "numb": 29,
+    "question": "A sampling procedure such that each possible combination of sampling units out of the population has the same chance of being selected is referred to as",
+    "answer": "simple random sampling",
+    "options": [
+      "simple random sampling",
+      "systematic sampling",
+      "stratified sampling",
+      "multistage sampling"
+    ]
+}
+,
+{
+    "numb": 30,
+    "question": "Which of these is true?",
+    "answer": "a or b",
+    "options": [
+      "physiological longevity > ecological longevity",
+      "physiological longevity = ecological longevity",
+      "physiological longevity < ecological longevity",
+      "a or b"
+    ]
+}]
