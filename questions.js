465a34504765008cc14035a4782b0e57f73cc1e7
Shubham soni <ss4185667@gmail.com>
2023-10-26 16:48:24 +0530

    module1_question_inserted

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which of these is not a characteristic of fitness?",
+    answer: "Fitness is environment-specific.",
+    options: [
+      "Fitness is environment-specific.",
+      "Fitness is species-specific.",
+      "Higher reproductive rate means higher fitness.",
+      "Fitness should be measured across several generations."
+    ]
+  },
+  {
+    numb: 2,
+    question: "Ecology is the scientific study of interactions among organisms and their ___. (Fill in the blanks)",
+    answer: "environment",
+    options: [
+      "habitat",
+      "niche",
+      "environment",
+      "population"
+    ]
+  },
+  {
+    numb: 3,
+    question: "\"Enquiry into plants\" is a book written by",
+    answer: "Theophrastus",
+    options: [
+      "Theophrastus",
+      "Linnaeus",
+      "Malthus",
+      "Humboldt"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of these is not a kind of selection",
+    answer: "stochastic",
+    options: [
+      "directional",
+      "stochastic",
+      "disruptive",
+      "stabilising"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Ecology is the scientific study of _______ that determine the distribution and abundance of organisms. (Fill in the blanks)",
+    answer: "interactions",
+    options: [
+      "statics",
+      "interactions",
+      "dynamics",
+      "habitat"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of these is not a step in natural selection?",
+    answer: "underpopulation",
+    options: [
+      "variation",
+      "underpopulation",
+      "struggle for existence",
+      "survival of the fittest"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of these is not a characteristic of fitness?",
+    answer: "Fitness works on traits such as size and speed.",
+    options: [
+      "Fitness is environment-specific.",
+      "Fitness is species-specific.",
+      "Fitness works on traits such as size and speed.",
+      "Fitness should be measured across several generations."
+    ]
+  },
+  {
+    numb: 8,
+    question: "In the Greek word root of Ecology, Oikos refers to",
+    answer: "household",
+    options: [
+      "household",
+      "preservation",
+      "environment",
+      "study"
+    ]
+  },
+  {
+    numb: 9,
+    question: "In the Greek word root of Ecology, logos refers to",
+    answer: "study",
+    options: [
+      "household",
+      "preservation",
+      "environment",
+      "study"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who amongst these is considered the father of Biogeography?",
+    answer: "Humboldt",
+    options: [
+      "Theophrastus",
+      "Linnaeus",
+      "Malthus",
+      "Humboldt"
+    ]
+  }
+];
