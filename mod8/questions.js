6771753105fda0fef51c6daa767b76875a2b9135
Shubham soni <ss4185667@gmail.com>
2023-10-27 22:53:11 +0530

    mod8

---
+++ mod8/questions.js
@@ -0,0 +1,110 @@
+let questions = [
+  {
+    "numb": 21,
+    "question": "Captive breeding is an example of",
+    "answer": "ex-situ conservation",
+    "options": [
+      "in-situ conservation",
+      "ex-situ conservation",
+      "in-situ preservation",
+      "ex-situ preservation"
+    ]
+  },
+  {
+    "numb": 22,
+    "question": "The \"subset of physical and biotic environmental factors that permit an animal (or plant) or survive and reproduce\" is the definition of",
+    "answer": "habitat",
+    "options": [
+      "habitat",
+      "ecosystem",
+      "biome",
+      "biosphere"
+    ]
+  },
+  {
+    "numb": 23,
+    "question": "We prefer those areas for the creation of a conservation reserve where the level of threat is",
+    "answer": "medium",
+    "options": [
+      "very high",
+      "medium",
+      "very low",
+      "non-existent"
+    ]
+  },
+  {
+    "numb": 24,
+    "question": "Which of these is a deterministic factor",
+    "answer": "death rate",
+    "options": [
+      "environmental variation",
+      "forest fire",
+      "death rate",
+      "diseases"
+    ]
+  },
+  {
+    "numb": 25,
+    "question": "The acronym HIPPO does not include",
+    "answer": "habitat enhancement",
+    "options": [
+      "habitat loss",
+      "habitat enhancement",
+      "invasive species",
+      "human overpopulation"
+    ]
+  },
+  {
+    "numb": 26,
+    "question": "Which of these correctly represents the process of habitat fragmentation and loss",
+    "answer": "original forest --> dissection --> perforation --> fragmentation --> attrition",
+    "options": [
+      "original forest --> dissection --> attrition --> fragmentation --> perforation",
+      "original forest --> dissection --> perforation --> attrition --> fragmentation",
+      "original forest --> dissection --> fragmentation --> perforation --> attrition"
+    ]
+  },
+  {
+    "numb": 27,
+    "question": "The acronym HIPPO does not include",
+    "answer": "pollination",
+    "options": [
+      "habitat loss",
+      "invasive species",
+      "pollination",
+      "pollution"
+    ]
+  },
+  {
+    "numb": 28,
+    "question": "Which of these is a stochastic factor",
+    "answer": "environmental fluctuations",
+    "options": [
+      "birth rate",
+      "death rate",
+      "population structures",
+      "environmental fluctuations"
+    ]
+  },
+  {
+    "numb": 29,
+    "question": "According to Leopold, which of these in not a tool of habitat management",
+    "answer": "sickle",
+    "options": [
+      "fire",
+      "gun",
+      "cattle",
+      "sickle"
+    ]
+  },
+  {
+    "numb": 30,
+    "question": "Zoo is an example of",
+    "answer": "ex-situ conservation",
+    "options": [
+      "in-situ conservation",
+      "ex-situ conservation",
+      "in-situ preservation",
+      "ex-situ preservation"
+    ]
+  }]
