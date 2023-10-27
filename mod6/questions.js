9ecf6cc670a37016998ee1e2a0c0d27031a03631
Shubham soni <ss4185667@gmail.com>
2023-10-27 14:38:55 +0530

    mod-6

---
+++ mod6/questions.js
@@ -0,0 +1,120 @@
+let questions = [{
+  "numb": 1,
+  "question": "Importance value varies from",
+  "answer": "0 to 300",
+  "options": [
+    "0 to 10",
+    "0 to 50",
+    "0 to 100",
+    "0 to 300"
+  ]
+},
+
+{
+  "numb": 2,
+  "question": "A species found most frequently in a particular community, but also present occasionally in others is called",
+  "answer": "selective species",
+  "options": [
+    "accidental species",
+    "indifferent species",
+    "selective species",
+    "exclusive species"
+  ]
+},
+
+{
+  "numb": 3,
+  "question": "Which of these depicts correctly the lithosere primary succession?",
+  "answer": "rock --> crustose lichen --> foliose lichen --> moss --> herbaceous stage --> shrub --> woodland --> climax",
+  "options": [
+    "rock --> foliose lichen --> crustose lichen --> moss --> herbaceous stage --> shrub --> woodland -> climax",
+    "moss --> crustose lichen --> foliose lichen --> rock --> herbaceous stage --> shrub --> woodland --> climax",
+    "rock --> crustose lichen --> foliose lichen --> shrub --> herbaceous stage --> moss --> woodland --> climax",
+    "rock --> crustose lichen --> foliose lichen --> moss --> herbaceous stage --> shrub --> woodland --> climax"
+  ]
+} ,
+
+{
+  "numb": 4,
+  "question": "Importance value can be written as",
+  "answer": "relative density + relative frequency + relative dominance",
+  "options": [
+    "relative density + relative frequency X relative dominance",
+    "relative density X relative frequency + relative dominance",
+    "relative density + relative frequency + relative dominance",
+    "relative density X relative frequency X relative dominance"
+  ]
+},
+
+{
+  "numb": 5,
+  "question": "Lithosere is an example of",
+  "answer": "xerosere",
+  "options": [
+    "hydrosere",
+    "xerosere",
+    "psammosere",
+    "halosere"
+  ]
+}
+,
+{
+  "numb": 6,
+  "question": "The climax near Tindi village is being controlled by disturbance by cattle. This is an example of",
+  "answer": "disclimax",
+  "options": [
+    "climatic climax",
+    "edaphic climax",
+    "disclimax",
+    "catastrophic climax"
+  ]
+}
+,
+{
+  "numb": 7,
+  "question": "A climax caused by wildfires is an example of",
+  "answer": "catastrophic climax",
+  "options": [
+    "climatic climax",
+    "edaphic climax",
+    "disclimax",
+    "catastrophic climax"
+  ]
+}
+,
+{
+  "numb": 8,
+  "question": "Which of these is correct?",
+  "answer": "a or b",
+  "options": [
+    "fundamental niche > realised niche",
+    "fundamental niche = realised niche",
+    "fundamental niche < realised niche",
+    "a or b"
+  ]
+}
+,
+{
+  "numb": 9,
+  "question": "When compared to generalist species, specialist species have",
+  "answer": "narrower niche",
+  "options": [
+    "narrower niche",
+    "broader niche",
+    "same size niche",
+    "none of these"
+  ]
+}
+,
+{
+  "numb": 10,
+  "question": "Which of these is not a characteristic of pioneer species",
+  "answer": "large size",
+  "options": [
+    "ability to grow on bare rocks",
+    "ability to tolerate extreme temperatures",
+    "large size",
+    "short life span"
+  ]
+}
+    ]
