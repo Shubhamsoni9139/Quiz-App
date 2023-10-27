379f237ec161ca35867c0ec871c69d0b5e3734d3
Shubham soni <ss4185667@gmail.com>
2023-10-27 23:01:15 +0530

    mod-9

---
+++ mod9/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+    "numb": 31,
+    "question": "Which of these is a preventive check according to Malthus?",
+    "answer": "foresight",
+    "options": [
+      "foresight",
+      "vice",
+      "misery",
+      "flood"
+    ]
+  },
+  {
+    "numb": 32,
+    "question": "Which of these is not a pillar of sustainability?",
+    "answer": "trans-boundary sustainability",
+    "options": [
+      "environmental sustainability",
+      "economic sustainability",
+      "trans-boundary sustainability",
+      "social sustainability"
+    ]
+  },
+  {
+    "numb": 33,
+    "question": "____ is to identify which potential impacts are relevant to assess (fill in the blank)",
+    "answer": "scoping",
+    "options": [
+      "screening",
+      "scoping",
+      "reporting",
+      "review"
+    ]
+  },
+  {
+    "numb": 34,
+    "question": "Which of these is a positive check according to Malthus?",
+    "answer": "war",
+    "options": [
+      "late marriage",
+      "war",
+      "celibacy",
+      "moral restraint"
+    ]
+  },
+  {
+    "numb": 35,
+    "question": "Which of these is a pillar of sustainability?",
+    "answer": "social sustainability",
+    "options": [
+      "social sustainability",
+      "industrial sustainability",
+      "agricultural sustainability",
+      "trans-boundary sustainability"
+    ]
+  },
+  {
+    "numb": 36,
+    "question": "The demographic transition sees a society move from",
+    "answer": "high birth rate, high death rate to low birth rate, low death rate",
+    "options": [
+      "high birth rate, low birth rate to low birth rate, low death rate",
+      "low birth rate, high death rate to low birth rate, low death rate",
+      "high birth rate, high death rate to low birth rate, low death rate",
+      "high birth rate, high death rate to low birth rate, high death rate"
+    ]
+  },
+  {
+    "numb": 37,
+    "question": "The book \"An Essay on the Principle of Population\" was written by",
+    "answer": "Malthus",
+    "options": [
+      "Darwin",
+      "Malthus",
+      "Spencer",
+      "Owens"
+    ]
+  },
+  {
+    "numb": 38,
+    "question": "The quantum of human impacts is given by",
+    "answer": "I = P X A X T",
+    "options": [
+      "I = P + A + T",
+      "I = P + A - T",
+      "I = P - (A + T)",
+      "I = P X A X T"
+    ]
+  },
+  {
+    "numb": 39,
+    "question": "____ determines which projects or development require a full or partial impact assessment study (fill in the blank)",
+    "answer": "screening",
+    "options": [
+      "screening",
+      "scoping",
+      "reporting",
+      "review"
+    ]
+  },
+  {
+    "numb": 40,
+    "question": "According to the Malthusian model",
+    "answer": "population grows in geometric progression, food supply increases in arithmetic progression",
+    "options": [
+      "population grows in geometric progression, food supply increases in geometric progression",
+      "population grows in arithmetic progression, food supply increases in arithmetic progression",
+      "population grows in arithmetic progression, food supply increases in geometric progression",
+      "population grows in geometric progression, food supply increases in arithmetic progression"
+    ]
+  }
+]
