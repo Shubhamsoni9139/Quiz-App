2e67c5014f1b5e3a2fcd9eb4222b79029c4e13b1
Shubham soni <ss4185667@gmail.com>
2023-10-27 16:11:20 +0530

    mod7

---
+++ mod7/questions.js
@@ -0,0 +1,119 @@
+let questions = [{
+  "numb": 1,
+  "question": "Scarcity of food is a",
+  "answer": "push factor",
+  "options": [
+    "chemical factor",
+    "demographic factor",
+    "push factor",
+    "pull factor"
+  ]
+}
+,
+{
+  "numb": 2,
+  "question": "Good climate is a",
+  "answer": "pull factor",
+  "options": [
+    "chemical factor",
+    "demographic factor",
+    "push factor",
+    "pull factor"
+  ]
+}
+,
+{
+  "numb": 3,
+  "question": "The movement of individuals away from their place of birth or hatching or seed production into a new habitat or area to survive and reproduce is called",
+  "answer": "dispersal",
+  "options": [
+    "translocation",
+    "migration",
+    "dispersal",
+    "drifting"
+  ]
+}
+,
+{
+  "numb": 4,
+  "question": "\"The geographical distribution of a species will be controlled by that environmental factor for which the organism has the narrowest range of tolerance.\" This is the statement for",
+  "answer": "Shelford’s law of tolerance",
+  "options": [
+    "Liebig’s law of the minimum",
+    "Liebig’s law of the maximum",
+    "Shelford’s law of tolerance",
+    "Shelford’s law of intolerance"
+  ]
+}
+,
+{
+  "numb": 5,
+  "question": "\"Quick movement over large distances, often across unsuitable terrain\" is a description of",
+  "answer": "jump dispersal",
+  "options": [
+    "diffusion",
+    "secular dispersal",
+    "jump dispersal",
+    "drifting"
+  ]
+}
+,
+{
+  "numb": 6,
+  "question": "The regular, seasonal movement of animals, often along fixed routes is called",
+  "answer": "migration",
+  "options": [
+    "translocation",
+    "migration",
+    "dispersal",
+    "drifting"
+  ]
+}
+,
+{
+  "numb": 7,
+  "question": "The movement of lions across the Gir landscape is an example of",
+  "answer": "diffusion",
+  "options": [
+    "diffusion",
+    "secular dispersal",
+    "jump dispersal",
+    "drifting"
+  ]
+}
+,
+{
+  "numb": 8,
+  "question": "Which of these is not a physical factor of habitat?",
+  "answer": "predators",
+  "options": [
+    "soil",
+    "moisture",
+    "predators",
+    "temperature"
+  ]
+}
+,
+{
+  "numb": 9,
+  "question": "Transplantation experiments are used to find",
+  "answer": "potential range",
+  "options": [
+    "potential range",
+    "effective range",
+    "actual range",
+    "economic range"
+  ]
+}
+,
+{
+  "numb": 10,
+  "question": "I tried growing vegetables under my teak plantation, but the vegetable plants died out. I should be concerned about",
+  "answer": "allelopathy",
+  "options": [
+    "autophagy",
+    "allelophagy",
+    "autopathy",
+    "allelopathy"
+  ]
+}  ]
